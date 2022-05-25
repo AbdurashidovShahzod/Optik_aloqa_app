2b28be0c8d10abc34c7f6b0dcdcea541e6a4b706
AbdurashidovShahzod <shahzod9933@gmail.com>
2022-05-25 17:44:48 +0500

    Demo Activity for kotlin coroutine

---
+++ app/src/main/java/uz/unzosoft/optikaloqaapp/app/DemoActivity.kt
@@ -0,0 +1,12 @@
+package uz.unzosoft.optikaloqaapp.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.unzosoft.optikaloqaapp.R
+
+class DemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
+    }
+}
