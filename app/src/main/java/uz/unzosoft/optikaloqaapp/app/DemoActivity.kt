d835741f4f8d4ce0bfa805266fa8ef53244e498e
AbdurashidovShahzod <shahzod9933@gmail.com>
2022-05-26 17:09:20 +0500

    Demo Activity for kotlin coroutine

--- app/src/main/java/uz/unzosoft/optikaloqaapp/app/DemoActivity.kt
+++ app/src/main/java/uz/unzosoft/optikaloqaapp/app/DemoActivity.kt
@@ -2,11 +2,42 @@ package uz.unzosoft.optikaloqaapp.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.*
 import uz.unzosoft.optikaloqaapp.R
+import java.util.*
 
 class DemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo)
     }
+}
+
+/**
+fun main(args: Array<String>): Unit = runBlocking {
+    repeat(100) {
+        launch {
+            val doWork = doWork("Abdurashidov Shahzod")
+            println(doWork + it.toString())
+        }
+    }
+}
+ */
+
+
+fun main(args: Array<String>): Unit = runBlocking {
+
+    val coroutine:List<Deferred<String>> = List(100){
+        async(start = CoroutineStart.LAZY) {
+            doWork("Abdurashidov Shahzod $it")
+        }
+    }
+    coroutine.forEach { println(it.await()) }
+
+}
+
+
+suspend fun doWork(str: String): String {
+    delay(Random().nextInt(5000).toLong())
+    return "Coroutine ishlayabdi ---->>> $str"
 }
